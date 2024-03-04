e14733b7be7ac529250c4aa1d85f08b26f3d5428
Kolyamilk <Kolyamilk@mail.ru>
2024-03-04 18:20:20 +0300

    create a project

---
+++ vite-project/src/API.jsx
@@ -0,0 +1,58 @@
+import md5 from 'md5'
+import { useState } from 'react'
+
+export default function API() {
+    const [name, setName] = useState('Загрузка...')
+    let date = new Date()
+    let year = date.getFullYear()
+    let day = date.getDate()
+    let mounth = date.getMonth() + 1
+    if (mounth < 10) mounth = '0' + mounth
+    if (day < 10) day = '0' + date.getDate()
+    let xAuth = 'Valantis_' + year + mounth + day
+    const URL = 'http://api.valantis.store:40000/'
+
+    async function sendRequest(method, url) {
+        return fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth': md5(xAuth)
+            },
+            //### Методы.
+            body: JSON.stringify({
+                "action": "get_items",
+                "params": {
+                    "ids": [
+                        "18e4e3bd-5e60-4348-8c92-4f61c676be1f",
+                        "1789ecf3-f81c-4f49-ada2-83804dcc74b0",
+
+                    ]
+                }
+            })
+        })
+            .then(response => {
+                return response.json()
+            })
+    }
+    sendRequest('POST', URL)
+        .then(response => getProduct(response.result[0])
+        )
+    function getProduct(product) {
+
+
+        let nameProduct = ' Название: ' + product.product
+        let brandProduct = ' Бренд: ' + product.brand
+        let priceProduct = ' Цена: ' + product.price
+        let idProduct = ' ID: ' + product.id
+
+        let allProduct = nameProduct + priceProduct + idProduct + brandProduct
+        setName(allProduct)
+
+    }
+    return (
+        <button>{name}</button>
+        // <button>asdasd</button>,
+        // <button>asdasd</button>
+    )
+}
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import API from './API'
+import md5 from 'md5'
+
+
+import './App.css'
+
+function App() {
+
+  return (
+
+    <>
+
+      <h1>Товары</h1>
+      <p> <API /></p>
+    </>
+  )
+}
+export default App
