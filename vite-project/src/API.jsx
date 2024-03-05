d9de12c857ffd630dcb82bf404f5bf73af9fad82
Kolyamilk <Kolyamilk@mail.ru>
2024-03-05 16:23:30 +0300

    update API.jsx Сделал вывод всех ID

--- vite-project/src/API.jsx
+++ vite-project/src/API.jsx
@@ -2,7 +2,10 @@ import md5 from 'md5'
 import { useState } from 'react'
 
 export default function API() {
-    const [name, setName] = useState('Загрузка...')
+    const [name, setName] = useState(['Обновить товары'])
+    const [ids, setIds] = useState([])
+
+    let productList = []
     let date = new Date()
     let year = date.getFullYear()
     let day = date.getDate()
@@ -11,8 +14,8 @@ export default function API() {
     if (day < 10) day = '0' + date.getDate()
     let xAuth = 'Valantis_' + year + mounth + day
     const URL = 'http://api.valantis.store:40000/'
-
-    async function sendRequest(method, url) {
+    //Вывод всех ID 
+    async function sendRequest(method, url, id) {
         return fetch(url, {
             method: method,
             headers: {
@@ -21,37 +24,84 @@ export default function API() {
             },
             //### Методы.
             body: JSON.stringify({
-                "action": "get_items",
-                "params": {
-                    "ids": [
-                     
-                        "1789ecf3-f81c-4f49-ada2-83804dcc74b0"
-
-                    ]
-                }
+                "action": "get_ids",
+                "params": { "limit": 1 }
             })
         })
             .then(response => {
                 return response.json()
             })
     }
-    sendRequest('POST', URL)
-        .then(response => getProduct(response.result[0])
-        )
-
-    async function getProduct(product) {
-        let nameProduct = ' Название: ' + product.product
-        let brandProduct = ' Бренд: ' + product.brand
-        let priceProduct = ' Цена: ' + product.price
-        let idProduct = ' ID: ' + product.id
-
-        let allProduct = nameProduct + priceProduct + idProduct + brandProduct
-        setName(allProduct)
-
+    //Получение ID и вывод товара с полями
+    async function showProducts(method, url, id) {
+        return fetch(url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth': md5(xAuth)
+            },
+            body: JSON.stringify({
+                "action": "get_items",
+                "params": { "ids": id },
+            })
+        })
+            .then(response => {
+                return response.json()
+            })
     }
-    return (
-      <p>{name}</p>
+    async function handleClick(item) {
+        setName('Обновляем...')
 
+        await sendRequest('POST', URL)
+            .then(response => getProduct(response.result)
+            )
+
+
+
+
+        //получить id и отправить в showproduct
+
+        async function getProduct(product) {
+            await showProducts('POST', URL, product)
+                .then(response => show(response)
+                )
+            async function show({ result }) {
+                for (let i = 0; i < result.length; i++) {
+                    let idProd = [result[i].id]
+                    let product = [result[i].product]
+                    let price = [result[i].price]
+
+                    item = [...idProd, ...product, ...price]
+
+                    productList.push([...item])
+                    // item.map(() => {
+                    //     console.log(item);
+
+                    // })
+                    setIds(productList)
+                    setName('Обновить')
+                }
+
+
+
+
+            }
+
+
+        }
+    }
+
+    return (
+        <>
+            <section>
+                <button onClick={handleClick}>{name}</button>
+                <ul>
+                    <li>{[...ids]}</li>
+                    <li>{[...ids]}</li>
+                </ul>
+
+            </section>
+        </>
         // <button>asdasd</button>,
         // <button>asdasd</button>
     )
--- vite-project/src/Header.jsx
+++ vite-project/src/Header.jsx
@@ -1,3 +1,5 @@
-export default function Header(){
-    ''
+export default function Header() {
+    return (
+        <h1>Товары</h1>
+    )
 }
